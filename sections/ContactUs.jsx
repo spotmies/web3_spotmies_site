028a4b516dc2c2828d56286e79acfe5789ccb307
sekharjavvadi <sekharatece@gmail.com>
2022-12-18 08:35:05 +0530

    contact us api implemented

--- sections/ContactUs.jsx
+++ sections/ContactUs.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 ("use client");
 
 import { motion } from "framer-motion";
@@ -12,7 +12,54 @@ import styles from "../styles";
 import { staggerContainer, fadeIn } from "../utils/motion";
 import { TitleText, TypingText } from "../components";
 import { socials } from "../constants";
+import axios from "axios";
 export default function ContactUs() {
+  // create a react ref for the contact us every input field
+  const nameRef = React.useRef(null);
+  const emailRef = React.useRef(null);
+  const messageRef = React.useRef(null);
+  const subjectRef = React.useRef(null);
+  const mobileRef = React.useRef(null);
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = (e) => {
+    e?.preventDefault();
+    setLoading(true);
+
+    const contact = {
+      subject: subjectRef?.current?.value,
+      suggestionFor: "contactUs",
+      suggestionFrom: "spotmies",
+      name: nameRef?.current?.value,
+      email: emailRef?.current?.value,
+      body: messageRef?.current?.value,
+      mobile: mobileRef?.current?.value,
+    };
+
+    axiosPostApiCall(contact);
+  };
+
+  const axiosPostApiCall = (body) => {
+    setLoading(true);
+    setIsFormSubmitted(false);
+    axios
+      .post(
+        "https://acchedya-server-production.up.railway.app/api/suggestion/new-suggestion",
+        body
+      )
+      .then((res) => {
+        console.log(res);
+        setLoading(false);
+        setIsFormSubmitted(true);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+        alert("Something went wrong");
+      });
+  };
+
   const getInTouch = (
     <motion.div
       variants={fadeIn("right", "tween", 0.2, 1)}
@@ -39,15 +86,18 @@ export default function ContactUs() {
 
       <p className="text-white font-bold text-md mt-[30px]">Follow us</p>
       <div className="flex gap-6 mt-[15px]">
-        {socials.map((social) => social.name != "github" && (
-          <img
-            onClick={() => window.open(social.link, "_blank")}
-            key={social.name}
-            src={social.url}
-            alt={social.name}
-            className="w-[30px] h-[30px] object-contain cursor-pointer opacity-70 hover:opacity-100"
-          />
-        ))}
+        {socials.map(
+          (social) =>
+            social.name != "github" && (
+              <img
+                onClick={() => window.open(social.link, "_blank")}
+                key={social.name}
+                src={social.url}
+                alt={social.name}
+                className="w-[30px] h-[30px] object-contain cursor-pointer opacity-70 hover:opacity-100"
+              />
+            )
+        )}
       </div>
 
       {/* <div className="flex flex-row items-center mt-[15px] gap-[20px]">
@@ -86,13 +136,14 @@ export default function ContactUs() {
             className="w-[100%] md:w-[700px] h-fit md:h-[550px] mt-[10px] rounded-[10px] md:rounded-[20px] gradient-05 border-[1px] border-[#6A6A6A] border "
           >
             {/* <div className="w-[100%] md:w-[700px] h-fit md:h-[550px] mt-[10px] rounded-[10px] md:rounded-[20px] gradient-05 border-[1px] border-[#6A6A6A] border "> */}
-            <form>
+            <form onSubmit={handleSubmit}>
               <div className="flex flex-col md:flex-row gap-[15px] md:gap-[45px] mt-[30px] p-[20px] w-full justify-around">
                 <div className="flex flex-col items-start w-full">
                   <p className="text-slate-400 text-md font-semibold m-[5px]">
                     Name
                   </p>
                   <input
+                    ref={nameRef}
                     required
                     type="text"
                     placeholder="Your Name"
@@ -104,8 +155,9 @@ export default function ContactUs() {
                     Email
                   </p>
                   <input
+                    ref={emailRef}
                     required
-                    type="text"
+                    type="email"
                     placeholder="Your Email Address"
                     className="w-full outline-none gradient-05 rounded-[10px] p-[15px] bg-slate-900 text-white"
                   />
@@ -117,9 +169,9 @@ export default function ContactUs() {
                     Phone
                   </p>
                   <input
-                    required
+                    ref={mobileRef}
                     type="text"
-                    placeholder="Your Number"
+                    placeholder="Your Number (Optional)"
                     className="w-full outline-none gradient-05 rounded-[10px] p-[15px] bg-slate-900 text-white"
                   />
                 </div>
@@ -128,6 +180,7 @@ export default function ContactUs() {
                     Subject
                   </p>
                   <input
+                    ref={subjectRef}
                     required
                     type="text"
                     placeholder="What is the Subject"
@@ -140,19 +193,30 @@ export default function ContactUs() {
                   Message
                 </p>
                 <textarea
+                  ref={messageRef}
+                  required
                   type="text"
                   placeholder="Your Message"
                   rows={5}
                   className="w-full outline-none gradient-05 rounded-[10px] p-[10px] bg-slate-900 text-white"
                 />
               </div>
-              <div className="flex flex-row justify-start items-center w-full pt-[0px] p-[20px] ">
+              <div className="flex flex-row justify-start items-center w-full pt-[0px] p-[20px]">
                 <button
                   type="submit"
-                  className="border-[#273558] border-[3px] gradient-05 text-white pt-[10px] pb-[10px] font-bold p-[20px] rounded-[10px]"
+                  disabled={loading}
+                  className={` ${
+                    loading && "animate-bounce"
+                  } border-[#273558] border-[3px] gradient-05 text-white pt-[10px] pb-[10px] font-bold p-[20px] rounded-[10px]`}
                 >
                   Submit
                 </button>
+
+                {isFormSubmitted && (
+                  <p className="text-[#1bcc4c] text-md font-semibold m-[5px] pl-[10px]">
+                    Thank you 👍, We will get back to you soon.
+                  </p>
+                )}
               </div>
             </form>
             {/* </div> */}

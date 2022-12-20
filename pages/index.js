926ecde32afabfc950b9678412839d91bc6b252a
sekharjavvadi <sekharatece@gmail.com>
2022-12-20 15:43:26 +0530

    condition added for mobile and webview

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Footer, Navbar } from "../components";
 import {
   About,
@@ -18,47 +19,75 @@ import WhyYouChooseUs from "../sections/WhyYouChooseUs";
 import Work from "../sections/Work/Work";
 import Testimonials2 from "../sections/testimonials2";
 
-const Home = () => (
-  <div className="bg-primary-black overflow-hidden">
-    <Navbar />
-    <Hero />
-    <div className="relative">
-      <About />
-      <div className="gradient-03 z-0" />
-      <Explore />
-    </div>
-    <div className="relative">
-      <GetStarted />
-      <div className="gradient-04 z-0" />
-    </div>
+const mainUi = () => (
+  <>
+    <div className="bg-primary-black">
+      <Navbar />
+      <Hero />
+      <div className="relative">
+        <About />
+        <div className="gradient-03 z-0" />
+        <Explore />
+      </div>
+      <div className="relative">
+        <GetStarted />
+        <div className="gradient-04 z-0" />
+      </div>
 
-    <div className="relative">
-      <Feedback />
+      <div className="">
+        <Feedback />
 
-      <div className="gradient-04 z-0" />
-      <Work />
-    </div>
-    <div className="relative">
-      <World />
+        <div className="gradient-04 z-0" />
+        <Work />
+      </div>
+      <div className="relative">
+        <World />
 
-      <div className="gradient-04 z-0" />
-      <Technologies />
-    </div>
-    <div className="relative">
-      {/* <Testimonials /> */}
-      <Testimonials2 />
-      <WhatWeHave />
-      <div className="gradient-04 z-0" />
+        <div className="gradient-04 z-0" />
+        <Technologies />
+      </div>
+      <div className="relative">
+        <Testimonials2 />
+        <WhatWeHave />
+        <div className="gradient-04 z-0" />
 
-      <WhyYouChooseUs />
+        <WhyYouChooseUs />
+      </div>
+      <div className="relative">
+        <Team />
+        <div className="gradient-04 z-0" />
+        <ContactUs />
+      </div>
+      <Footer />
     </div>
-    <div className="relative">
-      <Team />
-      <div className="gradient-04 z-0" />
-      <ContactUs />
-    </div>
-    <Footer />
-  </div>
+  </>
 );
+
+function Home() {
+  const [screenType, setScreenType] = useState("mobile");
+  useEffect(() => {
+    const width = window.innerWidth;
+    if (width < 768) {
+      setScreenType("mobile");
+      // return "mobile";
+    } else if (width < 1024) {
+      setScreenType("tablet");
+      // return "tablet";
+    } else {
+      setScreenType("desktop");
+      // return "desktop";
+    }
+  }, []);
+
+  return (
+    <div>
+      {screenType === "desktop" ? (
+        <div className="bg-primary-black overflow-hidden">{mainUi()}</div>
+      ) : (
+        <div className="bg-primary-black overflow-hidden">{mainUi()}</div>
+      )}
+    </div>
+  );
+}
 
 export default Home;
--- sections/testimonials2.jsx
+++ sections/testimonials2.jsx
@@ -19,7 +19,7 @@ export default function Testimonials2() {
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
-      items: 2,
+      items: 1,
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },

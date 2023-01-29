4734d3d901104b108bdd22c52789fc791a6c061c
sekharjavvadi <sekharatece@gmail.com>
2023-01-29 21:49:03 +0530

    removed the up sticky arrow

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ const mainUi = () => (
     <>
 
         <div className="bg-primary-black">
-            <Link href="#"
+            {/* <Link href="#"
                     className="fixed bottom-0 right-8 text-white py-4">
                 <div className="bg-white p-4 rounded-full">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
@@ -34,7 +34,7 @@ const mainUi = () => (
                         <polyline points="5 12 12 5 19 12"></polyline>
                     </svg>
                 </div>
-            </Link>
+            </Link> */}
             <Navbar/>
             <Hero/>
             <div className="relative">
